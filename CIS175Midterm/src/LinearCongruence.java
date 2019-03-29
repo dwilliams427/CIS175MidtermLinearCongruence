0c0d78e94d3c343e9e9f52eeb33094c5c14c8a95
Daniel Williams <33042983+dwilliams427@users.noreply.github.com>
2019-03-28 18:58:06 -0700

    Initial Commit

---
+++ CIS175Midterm/src/LinearCongruence.java
@@ -0,0 +1,67 @@
+/*
+ * Name: Daniel Williams & Matthew
+ * Date: 03/28/19 --> 04/11/19
+ * 
+ * 
+ * 
+ * @version 1.0
+ * 
+ * UML
+ * +seed -- double
+ * +multiplier -- double
+ * +increment -- double
+ * modulus -- double
+ * 
+ * SetSeed(double): void
+ * RandGenerator(): double
+ * 
+ * 
+ */
+
+import java.util.Random;
+
+
+
+public class LinearCongruence {
+	static Random rand = new Random();
+	
+
+	public LinearCongruence() {
+		// TODO Auto-generated constructor stub
+		int seed = 1;
+		int mult = 0;
+		int inc = 0;
+		int mod = 0;
+		
+	}
+	
+	
+	public static double SetSeed(double s) {
+		
+		// let user set seed
+		
+		return s;
+	}
+	
+	public static int RandGenerator() {
+		
+		//do the calculations, generate rand numbers, print them out
+		
+		return 
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean [] usedNumbers = new boolean[800];
+		
+		while (usedNumbers[seed])
+		{
+			
+		}
+		
+		
+	}
+
+}
