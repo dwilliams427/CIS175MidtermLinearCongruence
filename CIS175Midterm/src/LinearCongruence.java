0779cb2473d414691ce2706a35ea52d0db47e161
Ambrose2018 <mambrose@avc.edu>
2019-04-09 07:03:16 -0700

    work-in-progress. DO NOT MERGE! Committing only to be able to come back to later.

--- CIS175Midterm/src/LinearCongruence.java
+++ CIS175Midterm/src/LinearCongruence.java
@@ -1,30 +1,33 @@
 /*
- * Name: Daniel Williams & Matthew
- * Date: 03/28/19 --> 04/11/19
- * 
- * 
- * 
- * @version 1.0
- * 
+ * Name: Daniel Williams & Matthew Ambrose
+ *
  * UML
  * +seed -- double
  * +multiplier -- double
  * +increment -- double
  * modulus -- double
- * 
+ *
  * SetSeed(double): void
  * RandGenerator(): double
- * 
- * 
+ *
  */
 
 import java.util.Random;
 
-
-
+/**
+ * CIS 175 MidTerm Project -- Pseudo-Random Number Sequence Generator using Linear Congruence
+ *
+ * @author Daniel Williams
+ * @author Matthew Ambrose
+ * @version 2.0
+ * Date: 03/28/19 --> 04/11/19
+ *
+ */
 public class LinearCongruence {
+
+
 	static Random rand = new Random();
-	
+
 
 	public LinearCongruence() {
 		// TODO Auto-generated constructor stub
@@ -32,36 +35,65 @@ public class LinearCongruence {
 		int mult = 0;
 		int inc = 0;
 		int mod = 0;
-		
+
 	}
-	
-	
+
+
 	public static double SetSeed(double s) {
-		
+
 		// let user set seed
-		
+
 		return s;
 	}
-	
+
 	public static int RandGenerator() {
-		
+
 		//do the calculations, generate rand numbers, print them out
-		
-		return 
+
+		return
 	}
-	
+
+	public class RndGen {
+
+		// private fields
+		private int multiplier; // = 40;
+		private int increment; 	// = 3641;
+		private int modulus; 	// = 729;
+		private int seed; 		// = 1;
+		private boolean[] numberIsUsed;	// size will be set in constructor to same size as modulus
+
+		public RndGen(int argMultiplier, int argIncrement, int argModulus, int argSeed) {
+			boolean[] numberIsUsed = new boolean[modulus];
+			this.multiplier
+		}
+
+		public RndGen() {
+			RndGen(40,3641,729,1)
+		}
+
+		public int getSeed() {
+			return this.seed;
+		}
+	}
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		boolean [] usedNumbers = new boolean[800];
-		
+
+		// These four initial values are set based on the example used in the project instructions.
+		int multiplier = 40;
+		int increment = 3641;
+		int modulus = 729;
+		int seed = 1;
+
+		// Initialize the program.
+		RandGenerator genX = new RandomGenerator(theMultiplier,theSeed,theIncrement,theModulus);
+
 		while (usedNumbers[seed])
 		{
-			
+
 		}
-		
-		
+
+
 	}
 
 }
