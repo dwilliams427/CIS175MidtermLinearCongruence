a0e67526acec6beb48be5904e6800f60b9298268
Daniel Williams <33042983+dwilliams427@users.noreply.github.com>
2019-04-15 21:02:57 -0700

    Version 2.0, Organized parameters
    moved everything around so each object is properly appointed with its correct methods

--- src/LinearCongruence.java
+++ src/LinearCongruence.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /*
  * Name: Daniel Williams & Matthew Ambrose
  * Date: 03/28/19 --> 04/11/19
@@ -33,35 +36,66 @@
  * 
  */
 
-
-
 public class LinearCongruence {
-	static RandomNumberGenerator  GenX =  new RandomNumberGenerator();									// 3 iterations of the formula
-	static RandomNumberGenerator  GenY =  new RandomNumberGenerator();
-	static RandomNumberGenerator  GenZ =  new RandomNumberGenerator();
-	static RandomNumberGenerator [] RandGenArray = new RandomNumberGenerator[3];
-	static int counter = 1;
+	static Scanner scan = new Scanner(System.in);
+	static Random rand = new Random();
+//	static RandomNumberGenerator  GenX =  new RandomNumberGenerator();									// 3 iterations of the formula
+//	static RandomNumberGenerator  GenY =  new RandomNumberGenerator();
+//	static RandomNumberGenerator  GenZ =  new RandomNumberGenerator();
+//	static RandomNumberGenerator [] RandGenArray = new RandomNumberGenerator[3];
+	
+	static private int result = 0;
+	static private int seed =1;
+	static private int mult = 40;
+	static private int inc = 3641;
+	static private int mod = 729;
 	
 
 	public LinearCongruence() {
 		// TODO Auto-generated constructor stub
-		RandGenArray[0] = GenX;																			//add each object to an array
-		RandGenArray[1] = GenY;
-		RandGenArray[2] = GenZ;
-	}
-	
-	
-	public static void GenerateRandNum() {		
-		System.out.println("Your parameters are: \n"													//display developer defines parameters
-				+ "Seed: " + GenX.GetSeed() + "\n"
-						+ "Multiplier: " + GenX.GetMult() + "\n"
-								+ "Increment: " + GenX.GetInc() + "\n"
-										+ "Modulus: " + GenX.GetMod() + "\n");
 		
-		for (int i =0; i < RandGenArray.length; i++) {
-			System.out.println("Gen " + counter + "\n");
-			RandGenArray[i].RandGenerator();
-			counter++;
-		}			
 	}
+	public LinearCongruence(int s, int multi, int i, int m) {
+		// TODO Auto-generated constructor stub
+		seed = s;
+		 mult = multi;
+		 inc = i;
+		 mod = m;
+	}
+	
+			// GETTERS/SETTERS vvvvv //
+	public static double GetSeed() {return seed;}								//GETTERS/SETTERS
+	public static void SetSeed(int s) {seed = s;}
+	public static double GetMult() {return mult;	}
+	public static void SetMult(int multi) {	mult = multi;}
+	public static double GetInc() {	return inc;	}
+	public static void SetInc(int i) {inc = i;}
+	public static double GetMod() {	return mod;	}
+	public static void SetMod(int m) {	mod = m;}
+	
+	public static void RandGenerator() {										//do the calculations, generate random numbers, print out each result
+		for (int i =0; i < mod; i++) {
+			result = (mult * seed + inc) % mod;
+			System.out.println("Result #" + i + ": " +  result);
+			seed = result;
+		}
+		System.out.println();
+		
+		
+	}
+	
+	
+//	public static void GenerateRandNum() {		
+//		System.out.println("Your parameters are: \n"													//display developer defines parameters
+//				+ "Seed: " + GenX.GetSeed() + "\n"
+//						+ "Multiplier: " + GenX.GetMult() + "\n"
+//								+ "Increment: " + GenX.GetInc() + "\n"
+//										+ "Modulus: " + GenX.GetMod() + "\n");
+//		
+//		for (int i =0; i < RandGenArray.length; i++) {
+//			System.out.println("Gen " + counter + "\n");
+//			RandGenArray[i].RandGenerator();
+//			counter++;
+//		}			
+//	}
 }
--- src/RandomNumberGenerator.java
+++ src/RandomNumberGenerator.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * 
  * 
  * 
- * @version 1.0
+ * @version 2.0
  * 
  * UML
  * +seed -- double
@@ -35,74 +35,66 @@ import java.util.Scanner;
  * 
  */
 
-
 public class RandomNumberGenerator {
 	static Scanner scan = new Scanner(System.in);
 	static Random rand = new Random();
-	static LinearCongruence lc = new LinearCongruence();
-	static char option = 'x';
-	static int result = 0;
-	static int seed =1;
-	static int mult = 40;
-	static int inc = 3641;
-	static int mod = 729;
+	static private LinearCongruence GenX =  new LinearCongruence();									// 3 iterations of the formula
+	static private LinearCongruence GenY =  new LinearCongruence();	
+	static private LinearCongruence GenZ =  new LinearCongruence();	
+	static private LinearCongruence [] RandGenArray = new LinearCongruence[3];
+	static private char option = 'x';
+	static private int counter = 1;
+	
 
 	public RandomNumberGenerator() {
 		// TODO Auto-generated constructor stub
-		
-		 mult = rand.nextInt();
-		 inc = rand.nextInt();
-		 mod = rand.nextInt();
+		RandGenArray[0] = GenX;																			//add each object to an array
+		RandGenArray[1] = GenY;
+		RandGenArray[2] = GenZ;
 	}
-	public RandomNumberGenerator(int s, int multi, int i, int m) {
-		// TODO Auto-generated constructor stub
-		seed = s;
-		 mult = multi;
-		 inc = i;
-		 mod = m;
-	}
+//	public RandomNumberGenerator(int s, int multi, int i, int m) {
+//		// TODO Auto-generated constructor stub
+//		seed = s;
+//		 mult = multi;
+//		 inc = i;
+//		 mod = m;
+//	}
 	
-	// GETTERS/SETTERS vvvvv //
-	public static double GetSeed() {return seed;}								//GETTERS/SETTERS
-	public static void SetSeed(int s) {seed = s;}
-	public static double GetMult() {return mult;	}
-	public static void SetMult(int multi) {	mult = multi;}
-	public static double GetInc() {	return inc;	}
-	public static void SetInc(int i) {inc = i;}
-	public static double GetMod() {	return mod;	}
-	public static void SetMod(int m) {	mod = m;}
+//	// GETTERS/SETTERS vvvvv //
+//	public static double GetSeed() {return seed;}								//GETTERS/SETTERS
+//	public static void SetSeed(int s) {seed = s;}
+//	public static double GetMult() {return mult;	}
+//	public static void SetMult(int multi) {	mult = multi;}
+//	public static double GetInc() {	return inc;	}
+//	public static void SetInc(int i) {inc = i;}
+//	public static double GetMod() {	return mod;	}
+//	public static void SetMod(int m) {	mod = m;}
 	
 	
-	public static void RandGenerator() {										//do the calculations, generate random numbers, print out each result
-		for (int i =0; i < mod; i++) {
-			result = (mult * seed + inc) % mod;
-			System.out.println("Result #" + i + ": " +  result);
-			seed = result;
-		}
-		System.out.println();
-		
-		
-	}
+//	public static void RandGenerator() {										//do the calculations, generate random numbers, print out each result
+//		for (int i =0; i < mod; i++) {
+//			result = (mult * seed + inc) % mod;
+//			System.out.println("Result #" + i + ": " +  result);
+//			seed = result;
+//		}
+//		System.out.println();
+//		
+//		
+//	}
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Welcome to Linear Congruence");
-		System.out.println("We will calculate a sequence of psuedorandom numbers.");
-		MainMenu();
-		scan.close();
-
-	}
+	
 	
 	public static void MainMenu() throws Exception{
 		
 			
-		System.out.println("R – Run 3 Simulations\r\n" + 						
+		System.out.println("==================================\n"
+				+ "R – Run 3 Simulations\r\n" + 						
 				"S – Change Seed\r\n" + 
 				"I – Change Increment\r\n" + 
 				"U – Change Multiplier\r\n" + 
 				"M – Change Modulus\r\n" + 
-				"X – Exit\n");
+				"X – Exit\n"
+				+ "==================================\n");
 		
 						////// MENU SWITCH ////
 		try {
@@ -114,7 +106,7 @@ public class RandomNumberGenerator {
 		}
 		switch (option) 
         {
-            case 'R': LinearCongruence.GenerateRandNum();
+            case 'R': GenerateRandNum();
             			MainMenu();
                      break;
             case 'S': ChangeSeed();
@@ -142,10 +134,14 @@ public class RandomNumberGenerator {
 	
 	public static void ChangeSeed() throws Exception {
 		
-		System.out.println("Enter a new Seed (X to exit)");							// repeats this line when exiting for some reason
+		System.out.println("Enter a new Seed (X to exit)");							// TODO: fix this: repeats this line when exiting for some reason
 		try {		
 			int newSeed = scan.nextInt();
-			SetSeed(newSeed);
+			for (int i =0; i < RandGenArray.length; i++) {							// sets user's number to new Seed for each Simulation object
+				RandGenArray[i].SetSeed(newSeed);
+				counter++;
+			}	
+			counter = 0;															//reset simulation counter
 //			scan.reset();	
 		
 		} catch (Exception e) {
@@ -159,7 +155,11 @@ public class RandomNumberGenerator {
 		System.out.println("Enter a new Increment (X to exit)");
 		try {		
 			int newInc = scan.nextInt();
-			SetInc(newInc);
+			for (int i =0; i < RandGenArray.length; i++) {
+				RandGenArray[i].SetInc(newInc);
+				counter++;
+			}	
+			counter = 0;
 //			scan.reset();	
 		
 		} catch (Exception e) {
@@ -173,7 +173,11 @@ public class RandomNumberGenerator {
 		System.out.println("Enter a new Multiplier (X to exit)");
 		try {		
 			int newMult = scan.nextInt();
-			SetMult(newMult);
+			for (int i =0; i < RandGenArray.length; i++) {
+				RandGenArray[i].SetMult(newMult);
+				counter++;
+			}	
+			counter = 0;
 //			scan.reset();	
 		
 		} catch (Exception e) {
@@ -187,7 +191,11 @@ public class RandomNumberGenerator {
 		System.out.println("Enter a new Modulus (X to exit)");
 		try {		
 			int newMod = scan.nextInt();
-			SetMod(newMod);
+			for (int i =0; i < RandGenArray.length; i++) {
+				RandGenArray[i].SetMod(newMod);
+				counter++;
+			}	
+			counter = 0;
 //			scan.reset();	
 		
 		} catch (Exception e) {
@@ -195,5 +203,33 @@ public class RandomNumberGenerator {
 			e.getMessage();
 		} 
 	}
+	
+	public static void GenerateRandNum() {		
+		System.out.println("Your parameters are: \n"													//display developer defines parameters
+				+ "Seed: " + GenX.GetSeed() + "\n"
+						+ "Multiplier: " + GenX.GetMult() + "\n"
+								+ "Increment: " + GenX.GetInc() + "\n"
+										+ "Modulus: " + GenX.GetMod() + "\n");
+		
+		for (int i =0; i < RandGenArray.length; i++) {
+			System.out.println("Gen " + counter + "\n");
+			RandGenArray[i].RandGenerator();
+			counter++;
+		}
+		counter =0;
+	}
+	
+//	private static void printRowOutlined(String left, int right) {
+//	    System.out.printf("%-15s%03d%n", left, right);
+//	}
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Linear Congruence");
+		System.out.println("We will calculate a sequence of psuedorandom numbers.");
+		MainMenu();
+		scan.close();
 
+	}
 }
